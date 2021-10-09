f203e4af367a3b2fa3d0ab55d6d58c58df1fb93b
Long Le <hjjddbz@gmail.com>
2021-10-09 09:36:40 +0700

    Added FacebookCrawlerPool

--- app/crawler/fb_crawler.py
+++ app/crawler/fb_crawler.py
@@ -1,26 +1,24 @@
 import os
 import sys
 from selenium import webdriver
-from bs4 import BeautifulSoup
-import json
-import ast 
-import logging
 import time
-import inspect
-import re
+import datetime
 import random
+import re
+import json
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 sys.path.append(os.getcwd())
-print(os.getcwd())
 from utils.extractor_utils import time_extractor
 from utils.log import create_log
+from config.fb_config import BaseConfig
+from data.db import crawled_post
 
 
 def wait(delta):
-    time.sleep(random.uniform(1, delta))
+    time.sleep(random.uniform(delta/2, delta))
 
 
 class FacebookCrawler:
@@ -54,11 +52,14 @@ class FacebookCrawler:
         driver = webdriver.Chrome(driver_path, options=options, desired_capabilities=desired_capabilities)
         # driver = webdriver.Chrome(ChromeDriverManager().install(), options=options, desired_capabilities=desired_capabilities)
 
+        self.config = BaseConfig
+
         self.username = username
         self.password = password
         self.articles = []
         self.driver = driver
         self.login()
+        self.valid = True
 
     
     def login(self):
@@ -74,39 +75,48 @@ class FacebookCrawler:
 
 
     def get_articles(self, user_id):
+        print(f'{self.username} is crawling user {user_id}...')
         wait(2)
         if 'https' in user_id:
             self.driver.get(user_id)
         self.driver.get(f"https://facebook.com/{user_id}")
-        SCROLL_PAUSE_TIME = 0.5
+        self.config.SCROLL_PAUSE_TIME = 0.5
         for _ in range(5):
             self.driver.execute_script(f"window.scrollTo(0, {(_+1)*540});")
-            time.sleep(SCROLL_PAUSE_TIME)
+            time.sleep(self.config.SCROLL_PAUSE_TIME)
 
         wait(5)
-        for i in range(1, 10):
+        for i in range(1, self.config.NUM_POSTS):
             url = ''
             try:
                 url = self.driver.find_element_by_xpath(f'/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[3]/div[{i}]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/span/span/span[2]/span/a')
             except Exception as ex:
-
-                url = self.driver.find_element_by_xpath(f'/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[2]/div[{i}]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/span/span/span[2]/span/a')
+                try:
+                    url = self.driver.find_element_by_xpath(f'/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[2]/div[{i}]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/span/span/span[2]/span/a')
             #                                         /html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/span/span/span[2]/span/a
             #                                         /html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[3]/div[1]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/span/span/span[2]/span/a
-            action = webdriver.ActionChains(self.driver)
-            action.move_to_element(url).key_down(Keys.CONTROL).click().perform()
-            wait(2)
-
-        current_window = self.driver.current_window_handle
+                except:
+                    pass
+            if url in crawled_post:
+                break
+            try:
+                action = webdriver.ActionChains(self.driver)
+                action.move_to_element(url).key_down(Keys.CONTROL).click().perform()
+                wait(2)
+            except AttributeError:
+                print(f'Account {self.username} has been blocked!')
+                with open('log.txt', 'w') as f:
+                    f.write(f'{str(datetime.datetime.utcnow())}: Account {self.username} has been blocked!\n')
+                self.valid = False
+                return
+        initial_window = self.driver.window_handles[0]
         for window in self.driver.window_handles:
-            if window == current_window:
-                continue
             self.driver.switch_to.window(window)
             url = str(self.driver.current_url)
-            if 'posts' not in url or 'watch' not in url:
+            if 'posts' not in url and 'watch' not in url:
                 continue
-            print(f'Crawling {url}')
-            wait(2)
+            print(f'\tCrawling post {url}...')
+            wait(self.config.POST_PAUSE_TIME)
 
             author = re.search('(?<=com/)([\w.])*', self.driver.current_url).group()
             content = ''
@@ -133,11 +143,46 @@ class FacebookCrawler:
                 'created_date':created_date,
                 'imgs':imgs
             })
+            wait(self.config.POST_PAUSE_TIME)
             self.driver.close()
+        self.driver.switch_to.window(initial_window)
+        self.export_articles(user_id)
         return 
 
 
     def export_articles(self, user_id, dir='output'):
-        with open(f"{dir}/{user_id} articles.json", "w", encoding="utf-8") as f:
+        with open(f"{dir}/{user_id}.json", "w", encoding="utf-8") as f:
             json.dump(self.articles, f, ensure_ascii=False)
 
+
+class FacebookCrawlerPool:
+
+    def __init__(self, user_file, target_file):
+        self.crawlers = []
+        self.__init_crawlers(user_file)
+        self.targets = open(target_file, 'r').read().split('\n')
+        
+    
+    def __init_crawlers(self, user_file):
+        with open(user_file, 'r') as f:
+            users = f.read().split('\n')
+            for user in users:
+                username = user.split()[0]
+                password = user.split()[1]
+                self.crawlers.append(FacebookCrawler(username, password))
+
+
+    def start(self):
+        id = 0
+        for target in self.targets:
+            crawler = self.crawlers[id]
+            while not crawler.valid:
+                print(f'Remove account {crawler.username}')
+                self.crawlers.remove(crawler)
+                id %= len(self.crawlers)
+                crawler = self.crawlers[id]
+            else:
+                self.crawlers[id].get_articles(target)
+                wait(30)
+                id += 1
+                id %= len(self.crawlers)
