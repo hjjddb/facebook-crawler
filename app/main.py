7ea6d71a40ef2113980fba7b65cb0e8e51afe651
Long Le <hjjddbz@gmail.com>
2021-10-09 09:42:49 +0700

    Updated main

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from crawler.fb_crawler import FacebookCrawler
+from crawler.fb_crawler import FacebookCrawlerPool
 
 from time import time, sleep
 from flask import Flask
@@ -7,11 +7,9 @@ from flask import Flask
 if __name__ == '__main__':
     # app = Flask(__name__)
     # app.run('0.0.0.0', 5001)
-    crawler = FacebookCrawler('100072986628485', '@@q3rajMm2524857')
-    ids = open('data/targets.txt', 'r').read().split('\n')
-    crawled = open('data/post.txt', 'r').read().split()
+
+    crawler = FacebookCrawlerPool('data/users.txt', 'data/targets.txt')
+    
     while True:
-        for id in ids:
-            print(f'Crawling {id}')
-            crawler.get_articles(id)
-            crawler.export_articles(id)
+        crawler.start()
+        time.sleep(1800)
